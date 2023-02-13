03d34d7e8b200bfb3a17c1fb9e32b87f6e1e1096
Iskandar <seik747@gmail.com>
2023-02-13 16:35:52 +0000

    starting something new

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,47 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar bg-secondary">
+      <div className="container">
+        <Link to="/" className="text-decoration-none">
+          <div className="navbar-brand text-white mb-0 h1 fw-bolder fst-italic">
+            Dota 2 personal guide
+          </div>
+        </Link>
+        <div className="ml-auto">
+          <Link to="/demo">
+            <div class="dropdown">
+              <button
+                class="btn btn-secondary dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                My Heroes
+              </button>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Champ1
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Champ2
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Champ3
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,31 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import catImg from "../../img/crying-cat-home.png";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="text-center mt-5">
+      <h1>Hello, mate!</h1>
+      <h3 className="my-4">
+        This site is an attempt to put in order some of the knowledge I have
+        about this cursed game in a desperate cry for help
+      </h3>
+      <p>
+        <img src={catImg} />
+      </p>
+      <div className="alert alert-info">
+        {store.message ||
+          "Loading message from the backend (make sure your python backend is running)..."}
+      </div>
+      <p>
+        This boilerplate comes with lots of documentation:{" "}
+        <a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
+          Read documentation
+        </a>
+      </p>
+    </div>
+  );
 };
